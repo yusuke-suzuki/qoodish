a4005de03f6ad9055f6df38ae0d38447ad64c4d2
Yusuke Suzuki <yusuke_suzuki@kogasoftware.com>
2017-09-07 18:06:45 +0900

    Skip subscribe request if existing IID

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -3,7 +3,9 @@ class DevicesController < ApplicationController
 
   def create
     ActiveRecord::Base.transaction do
-      current_user.devices.find_or_create_by!(
+      return if current_user.devices.exists?(registration_token: params[:registration_token])
+
+      current_user.devices.create!(
         registration_token: params[:registration_token]
       )
       current_user.following_maps.each do |map|
