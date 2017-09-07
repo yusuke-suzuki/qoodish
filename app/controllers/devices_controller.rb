c118faad58c37349981096dc235562b1f7eb17cd
Yusuke Suzuki <yusuke_suzuki@kogasoftware.com>
2017-09-07 13:34:58 +0900

    Subscribe existing topics on create new device

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -2,8 +2,13 @@ class DevicesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @current_user.devices.find_or_create_by!(
-      registration_token: params[:registration_token]
-    )
+    ActiveRecord::Base.transaction do
+      current_user.devices.find_or_create_by!(
+        registration_token: params[:registration_token]
+      )
+      current_user.following_maps.each do |map|
+        current_user.subscribe_topic("map_#{map.id}")
+      end
+    end
   end
 end
