96f00c120142cffdc1d53ed7ebcc7337e942b46a
Yusuke Suzuki <yusuke.suzuki.0707@gmail.com>
2018-01-07 18:52:52 +0900

    Fix API routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,29 @@
 Rails.application.routes.draw do
-  resources :users, only: [:show, :create, :destroy] do
-    scope module: :users do
-      resources :maps, only: [:index]
+  scope :api do
+    resources :users, only: [:show, :create, :destroy] do
+      scope module: :users do
+        resources :maps, only: [:index]
+      end
     end
-  end
-  resources :devices, only: [:create, :destroy]
-  resources :maps do
-    scope module: :maps do
-      resources :reviews, only: [:index, :show, :create]
-      resources :spots, only: [:index, :show]
-      resources :collaborators, only: [:index]
-      resource :follow, only: [:create, :destroy]
-      resource :metadata, only: [:show]
+    resources :devices, only: [:create, :destroy]
+    resources :maps do
+      scope module: :maps do
+        resources :reviews, only: [:index, :show, :create]
+        resources :spots, only: [:index, :show]
+        resources :collaborators, only: [:index]
+        resource :follow, only: [:create, :destroy]
+        resource :metadata, only: [:show]
+      end
     end
-  end
-  resources :reviews, only: [:index, :update, :destroy] do
-    scope module: :reviews do
-      resource :metadata, only: [:show]
-      resource :like, only: [:create, :destroy]
-      resources :likes, only: [:index]
+    resources :reviews, only: [:index, :update, :destroy] do
+      scope module: :reviews do
+        resource :metadata, only: [:show]
+        resource :like, only: [:create, :destroy]
+        resources :likes, only: [:index]
+      end
     end
+    resources :inappropriate_contents, only: [:create]
+    resources :places, only: [:index]
+    resources :notifications, only: [:index, :update]
   end
-  resources :inappropriate_contents, only: [:create]
-  resources :places, only: [:index]
-  resources :notifications, only: [:index, :update]
 end
