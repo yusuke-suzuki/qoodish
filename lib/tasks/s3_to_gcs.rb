09eaeba87b91dca74a6da8397d14889c35f1ca6c
Yusuke <yusuke.suzuki.0707@gmail.com>
2018-03-21 13:51:58 +0900

    Add task to migrate AWS S3 to Google Cloud Storage

---
+++ lib/tasks/s3_to_gcs.rb
@@ -0,0 +1,20 @@
+# bin/rails runner lib/tasks/s3_to_gcs.rb
+
+require 'google/cloud'
+require 'open-uri'
+
+gcloud = Google::Cloud.new ENV['FIREBACE_PROJECT_ID'], './gcp-credentials.json'
+storage = gcloud.storage
+bucket = storage.bucket ENV['FIREBASE_IMAGE_BUCKET_NAME']
+
+target_reviews = Review.where.like(image_url: '%aws%')
+target_reviews.each do |review|
+  s3_image_url = review.image_url
+  file_name = File.basename(s3_image_url)
+  open(s3_image_url, 'rb') do |data|
+    bucket.create_file(data, "images/#{file_name}", { content_type: 'image/jpeg' })
+  end
+  file = bucket.file "images/#{file_name}"
+  download_url = "https://firebasestorage.googleapis.com/v0/b/#{ENV['FIREBASE_IMAGE_BUCKET_NAME']}/o/images%2F#{file_name}?alt=media"
+  review.update!(image_url: download_url)
+end
