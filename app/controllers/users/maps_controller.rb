93e6afe3d82d3af566a308677a6dd8d0c64e22fd
Yusuke <yusuke.suzuki.0707@gmail.com>
2018-12-16 14:16:49 +0900

    Fetch user following maps API

--- app/controllers/users/maps_controller.rb
+++ app/controllers/users/maps_controller.rb
@@ -5,10 +5,18 @@ module Users
     def index
       @maps =
         if params[:user_id] == current_user.uid
-          current_user.maps.includes(:user, :reviews).order(created_at: :desc)
+          if params[:following]
+            current_user.following_maps.includes(:user, :reviews).order(created_at: :desc)
+          else
+            current_user.maps.includes(:user, :reviews).order(created_at: :desc)
+          end
         else
           user = User.find_by!(id: params[:user_id])
-          user.maps.referenceable_by(current_user)
+          if params[:following]
+            user.following_maps.referenceable_by(current_user)
+          else
+            user.maps.referenceable_by(current_user)
+          end
         end
     end
   end
