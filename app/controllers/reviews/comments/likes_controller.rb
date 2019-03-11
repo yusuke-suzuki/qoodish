69a74c7bb0e6bc4a4c1e49648710804d4677105c
Yusuke <yusuke.suzuki.0707@gmail.com>
2019-03-11 15:52:49 +0900

    Get comment likes API

--- app/controllers/reviews/comments/likes_controller.rb
+++ app/controllers/reviews/comments/likes_controller.rb
@@ -2,11 +2,19 @@ module Reviews
   module Comments
     class LikesController < ApplicationController
       before_action :authenticate_user!
-      before_action :require_sign_in!, only: [:create, :destroy]
+      before_action :require_sign_in!, only: %i[create destroy]
+
+      def index
+        comment = Comment.find_by!(id: params[:comment_id])
+        raise Exceptions::NotFound unless current_user.referenceable?(comment.review.map)
+
+        @likes = comment.get_likes
+      end
 
       def create
         @review = Review.includes(:map, :user, :comments).find_by!(id: params[:review_id])
         raise Exceptions::NotFound unless current_user.referenceable?(@review.map)
+
         comment = @review.comments.find(params[:comment_id])
 
         ActiveRecord::Base.transaction do
@@ -35,6 +43,7 @@ module Reviews
       def destroy
         @review = Review.includes(:map).find_by!(id: params[:review_id])
         raise Exceptions::NotFound unless current_user.referenceable?(@review.map)
+
         comment = @review.comments.find(params[:comment_id])
         comment.unliked_by(current_user)
       end
