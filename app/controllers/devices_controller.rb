a29dd00ad1f41b667965b70cc370aca299d1596e
Yusuke <yusuke.suzuki.0707@gmail.com>
2019-03-17 17:57:24 +0900

    Add callback on create/destroy device

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -9,22 +9,20 @@ class DevicesController < ApplicationController
         return
       end
 
-      Rails.logger.info("Create new registration token. registration_token: #{params[:registration_token]} uid: #{current_user.uid}")
       current_user.devices.create!(
         registration_token: params[:registration_token]
       )
-      current_user.following_maps.each do |map|
-        current_user.subscribe_topic("map_#{map.id}")
-      end
-      current_user.subscribe_topic("user_#{current_user.id}")
+      Rails.logger.info("Create new registration token. registration_token: #{params[:registration_token]} uid: #{current_user.uid}")
     end
   end
 
   def destroy
-    device = current_user.devices.find_by(registration_token: params[:id])
-    return if device.blank?
+    ActiveRecord::Base.transaction do
+      device = current_user.devices.find_by(registration_token: params[:id])
+      return if device.blank?
 
-    Rails.logger.info("Create new registration token. registration_token: #{params[:id]} uid: #{current_user.uid}")
-    device.destroy!
+      device.destroy!
+      Rails.logger.info("Deleted registration token: #{params[:id]} uid: #{current_user.uid}")
+    end
   end
 end
--- app/models/device.rb
+++ app/models/device.rb
@@ -11,4 +11,53 @@ class Device < ApplicationRecord
               scope: :user_id,
               strict: Exceptions::DuplicateRegistrationToken
             }
+
+  after_create :subscribe_topics
+  before_destroy :unsubscribe_topics
+
+  private
+
+  def topics
+    topics = ["user_#{user_id}"]
+    map_topics = user.following_maps.map do |map|
+      "map_#{map.id}"
+    end
+    topics += map_topics
+  end
+
+  def subscribe_topics
+    GoogleIidClient.configure do |config|
+      config.api_key['Authorization'] = "key=#{ENV['FCM_SERVER_KEY']}"
+      config.debugging = Rails.env.development?
+    end
+
+    api_instance = GoogleIidClient::RelationshipMapsApi.new
+
+    topics.each do |topic|
+      begin
+        result = api_instance.iid_v1_iid_token_rel_topics_topic_name_post(registration_token, topic)
+        Rails.logger.info(result)
+      rescue GoogleIidClient::ApiError => e
+        Rails.logger.error("Exception when calling RelationshipMapsApi->iid_v1batch_remove_post: #{e}")
+      end
+    end
+  end
+
+  def unsubscribe_topics
+    GoogleIidClient.configure do |config|
+      config.api_key['Authorization'] = "key=#{ENV['FCM_SERVER_KEY']}"
+      config.debugging = Rails.env.development?
+    end
+
+    api_instance = GoogleIidClient::RelationshipMapsApi.new
+
+    topics.each do |topic|
+      begin
+        result = api_instance.iid_v1_iid_token_rel_topics_topic_name_delete(registration_token, topic)
+        Rails.logger.info(result)
+      rescue GoogleIidClient::ApiError => e
+        Rails.logger.error("Exception when calling RelationshipMapsApi->iid_v1batch_remove_post: #{e}")
+      end
+    end
+  end
 end
