b05c17ebaae0b704e2aaa426a880ce3be2edd742
Yusuke <yusuke.suzuki.0707@gmail.com>
2019-04-04 17:28:29 +0900

    Handle routing error

--- config/routes.rb
+++ config/routes.rb
@@ -7,27 +7,27 @@ Rails.application.routes.draw do
       resource :push_notification, only: [:update]
     end
   end
-  resources :devices, only: [:update, :destroy]
+  resources :devices, only: %i[update destroy]
   resources :maps do
     scope module: :maps do
-      resources :reviews, only: [:index, :show, :create]
-      resources :spots, only: [:index, :show]
+      resources :reviews, only: %i[index show create]
+      resources :spots, only: %i[index show]
       resources :collaborators, only: [:index]
       resources :invites, only: [:create]
-      resource :follow, only: [:create, :destroy]
-      resource :like, only: [:create, :destroy]
+      resource :follow, only: %i[create destroy]
+      resource :like, only: %i[create destroy]
       resources :likes, only: [:index]
       resource :metadata, only: [:show]
     end
   end
-  resources :reviews, only: [:index, :update, :destroy] do
+  resources :reviews, only: %i[index update destroy] do
     scope module: :reviews do
       resource :metadata, only: [:show]
-      resource :like, only: [:create, :destroy]
+      resource :like, only: %i[create destroy]
       resources :likes, only: [:index]
-      resources :comments, only: [:create, :destroy] do
+      resources :comments, only: %i[create destroy] do
         scope module: :comments do
-          resource :like, only: [:create, :destroy]
+          resource :like, only: %i[create destroy]
           resources :likes, only: [:index]
         end
       end
@@ -35,12 +35,15 @@ Rails.application.routes.draw do
   end
   resources :inappropriate_contents, only: [:create]
   resources :places, only: [:index]
-  resources :spots, only: [:index, :show] do
+  resources :spots, only: %i[index show] do
     scope module: :spots do
       resources :reviews, only: [:index]
       resource :metadata, only: [:show]
     end
   end
-  resources :notifications, only: [:index, :update]
+  resources :notifications, only: %i[index update]
   resources :invites, only: [:index]
+
+  root 'application#routing_error'
+  get '*path' => 'application#routing_error'
 end
