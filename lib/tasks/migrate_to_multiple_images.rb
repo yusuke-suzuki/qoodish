35f183a0019415d3175c86d2041b030af81a8515
Yusuke <yusuke.suzuki.0707@gmail.com>
2020-02-11 19:54:41 +0900

    Fix migrate tasks for multiple images

--- lib/tasks/migrate_to_multiple_images.rb
+++ lib/tasks/migrate_to_multiple_images.rb
@@ -4,10 +4,10 @@ ActiveRecord::Base.transaction do
   Review.all.each do |review|
     next if review.images.present?
     next if review.image_url.blank?
+    next if review.images.exists?(url: review.image_url)
 
     review.images.create!(
       url: review.image_url
     )
-    sleep(1)
   end
 end
