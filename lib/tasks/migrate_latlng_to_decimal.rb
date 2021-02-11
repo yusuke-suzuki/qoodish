985e835f2efeb8fe5360510f9cff1fec8e84e09e
Yusuke Suzuki <yusuke.suzuki.0707@gmail.com>
2021-02-11 19:46:01 +0900

    tmp

--- lib/tasks/migrate_latlng_to_decimal.rb
+++ lib/tasks/migrate_latlng_to_decimal.rb
@@ -1,7 +1,7 @@
 class MigrateLatLngToDecimal
   def run
     ActiveRecord::Base.transaction do
-      PlaceDetail.all.each do |place_detail|
+      PlaceDetail.where("id > ?", 837).each do |place_detail|
         puts "[Migrate] Start processing #{place_detail.id}"
         puts "[Migrate] before lat: #{place_detail.latitude}, lng: #{place_detail.longitude}"
 
